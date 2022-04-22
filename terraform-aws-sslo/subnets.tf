b61a0184d019958ccd68375063fa4073befbdaa3
f5jason <f5jason@users.noreply.github.com>
2022-04-22 16:29:47 -0400

    added variables to subnets.tf

--- terraform-aws-sslo/subnets.tf
+++ terraform-aws-sslo/subnets.tf
@@ -1,10 +1,10 @@
 ## Create Management Subnet 
 resource "aws_subnet" "management" {
-  vpc_id                   = module.vpc.vpc_id
-  cidr_block               = "10.0.1.0/24"
-  availability_zone        = var.az
+  vpc_id            = module.vpc.vpc_id
+  cidr_block        = var.vpc_cidrs["management"]
+  availability_zone = var.az
   tags = {
-    Name = "${var.prefix}-sslo-management"
+    Name       = "${var.prefix}-sslo-management"
     Group_Name = "${var.prefix}-sslo-management"
   }
 }
@@ -12,11 +12,11 @@ resource "aws_subnet" "management" {
 
 ## Create External Subnet
 resource "aws_subnet" "external" {
-  vpc_id                = module.vpc.vpc_id
-  cidr_block            = "10.0.2.0/24"
-  availability_zone     = var.az
+  vpc_id            = module.vpc.vpc_id
+  cidr_block        = var.vpc_cidrs["external"]
+  availability_zone = var.az
   tags = {
-    Name = "${var.prefix}-sslo-external"
+    Name       = "${var.prefix}-sslo-external"
     Group_Name = "${var.prefix}-sslo-external"
   }
 }
@@ -24,11 +24,11 @@ resource "aws_subnet" "external" {
 
 ## Create DMZ1 Subnet
 resource "aws_subnet" "DMZ1" {
-  vpc_id                = module.vpc.vpc_id
-  cidr_block            = "10.0.3.0/25"
-  availability_zone     = var.az
+  vpc_id            = module.vpc.vpc_id
+  cidr_block        = var.vpc_cidrs["dmz1"]
+  availability_zone = var.az
   tags = {
-    Name = "${var.prefix}-DMZ1"
+    Name       = "${var.prefix}-DMZ1"
     Group_Name = "${var.prefix}-DMZ1"
   }
 }
@@ -36,11 +36,11 @@ resource "aws_subnet" "DMZ1" {
 
 ## Create DMZ2 Subnet
 resource "aws_subnet" "DMZ2" {
-  vpc_id                = module.vpc.vpc_id
-  cidr_block            = "10.0.3.128/25"
-  availability_zone     = var.az
+  vpc_id            = module.vpc.vpc_id
+  cidr_block        = var.vpc_cidrs["dmz2"]
+  availability_zone = var.az
   tags = {
-    Name = "${var.prefix}-DMZ2"
+    Name       = "${var.prefix}-DMZ2"
     Group_Name = "${var.prefix}-DMZ2"
   }
 }
@@ -48,11 +48,11 @@ resource "aws_subnet" "DMZ2" {
 
 ## Create DMZ3 Subnet
 resource "aws_subnet" "DMZ3" {
-  vpc_id                = module.vpc.vpc_id
-  cidr_block            = "10.0.4.0/25"
-  availability_zone     = var.az
+  vpc_id            = module.vpc.vpc_id
+  cidr_block        = var.vpc_cidrs["dmz3"]
+  availability_zone = var.az
   tags = {
-    Name = "${var.prefix}-DMZ3"
+    Name       = "${var.prefix}-DMZ3"
     Group_Name = "${var.prefix}-DMZ3"
   }
 }
@@ -60,11 +60,11 @@ resource "aws_subnet" "DMZ3" {
 
 ## Create DMZ4 Subnet
 resource "aws_subnet" "DMZ4" {
-  vpc_id                = module.vpc.vpc_id
-  cidr_block            = "10.0.4.128/25"
-  availability_zone     = var.az
+  vpc_id            = module.vpc.vpc_id
+  cidr_block        = var.vpc_cidrs["dmz4"]
+  availability_zone = var.az
   tags = {
-    Name = "${var.prefix}-DMZ4"
+    Name       = "${var.prefix}-DMZ4"
     Group_Name = "${var.prefix}-DMZ4"
   }
 }
@@ -72,11 +72,11 @@ resource "aws_subnet" "DMZ4" {
 
 ## Create Internal Subnet
 resource "aws_subnet" "internal" {
-  vpc_id                = module.vpc.vpc_id
-  cidr_block            = "10.0.5.0/24"
-  availability_zone     = var.az
+  vpc_id            = module.vpc.vpc_id
+  cidr_block        = var.vpc_cidrs["internal"]
+  availability_zone = var.az
   tags = {
-    Name = "${var.prefix}-internal"
+    Name       = "${var.prefix}-internal"
     Group_Name = "${var.prefix}-internal"
   }
 }
@@ -84,11 +84,11 @@ resource "aws_subnet" "internal" {
 
 ## Create TGW App Subnet
 resource "aws_subnet" "tgw-appstack" {
-  vpc_id                = aws_vpc.appstack.id
-  cidr_block            = "192.168.1.0/24"
-  availability_zone     = var.az
+  vpc_id            = aws_vpc.appstack.id
+  cidr_block        = var.vpc_cidrs["application"]
+  availability_zone = var.az
   tags = {
-    Name = "${var.prefix}-tgw-appstack"
+    Name       = "${var.prefix}-tgw-appstack"
     Group_Name = "${var.prefix}-tgw-appstack"
   }
 }
