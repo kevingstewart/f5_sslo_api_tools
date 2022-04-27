e7a5097d8854f5900bb42d7cfcf6de8bc56f262e
f5jason <f5jason@users.noreply.github.com>
2022-04-27 15:14:09 -0400

    update DO to 1.29

--- ansible/scripts/deploy_inbound_l3.sh
+++ ansible/scripts/deploy_inbound_l3.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 echo ""
 echo "This will deploy an SSL Orchestrator Inbound Layer 3 Topology"
-echo "using the ansible_vars.yaml file created by the accompanying Terraform."
+echo "using the variables file created by the accompanying Terraform."
+echo ""
+echo "Note: You must copy the ansible_vars.yaml file here first."
+echo ""
+echo "ansible_vars.yaml"
+echo "========================================================================"
+cat ansible_vars.yaml
+echo "========================================================================"
+echo "If you do not see any variables above, abort now!"
 echo ""
 read -r -p "Are you sure? [y/N] " response
 
